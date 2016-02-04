9c5d9f2efdc63b355b8937b9f36e1ca65dbedc4e
ei17ringo <ei17ringo@gmail.com>
2016-02-04 13:25:58 +0800

    Initial Commit

---
+++ sampleTabbedApplication/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  sampleTabbedApplication
+//
+//  Created by Eriko Ichinohe on 2016/02/04.
+//  Copyright © 2016年 Eriko Ichinohe. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ sampleTabbedApplication/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  sampleTabbedApplication
+//
+//  Created by Eriko Ichinohe on 2016/02/04.
+//  Copyright © 2016年 Eriko Ichinohe. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
